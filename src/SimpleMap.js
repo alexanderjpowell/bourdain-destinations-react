e1971d10e3f187c19bd379ad7df3aad598f2f07f
Alexander Powell <ajpowell@email.wm.edu>
2020-05-19 22:46:49 -0700

    filter pins by show

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+//import controllable from 'react-controllables';
 import GoogleMapReact from 'google-map-react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
 import MAPS_API_KEY from './config';
 
-const handleApiLoaded = (map, maps) => {
-	var legend = document.getElementById('legend');
-	map.controls[maps.ControlPosition.BOTTOM_CENTER].push(legend);
-	drawMarkers(map, maps, ['No Reservations', 'The Layover', 'Parts Unknown']);
-};
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-function drawMarkers(map, maps, shows) {
+/*const handleApiLoaded = (map, maps) => {
+	//var legend = document.getElementById('legend');
+	//map.controls[maps.ControlPosition.BOTTOM_CENTER].push(legend);
+	//drawMarkers(map, maps, ['No Reservations', 'The Layover', 'Parts Unknown']);
+};*/
+
+/*function drawMarkers(map, maps, shows) {
 	let url = "https://raw.githubusercontent.com/alexanderjpowell/bourdain-destinations-react/master/src/all.json";
 	fetch(url).then(response => response.json()).then(json => {
 		var marker;
@@ -39,7 +42,7 @@ function drawMarkers(map, maps, shows) {
 			}
 		}
 	}).catch(error => {alert(error);});
-}
+}*/
 
 const LegendStyle = {
 	margin: 10,
@@ -54,56 +57,103 @@ const ChipStyle = {
 
 class SimpleMap extends Component {
 
-	/*constructor(props) {
+	constructor(props) {
 		super(props);
-	}*/
+		this.state = {
+			//markers: [<AnyReactComponent lat={27.84} lng={11.11} text='Test' />],
+			markers: [],
+		};
+	}
 
-  static defaultProps = {
-    center: {
-      lat: 27.84,
-      lng: 11.11
-    },
-	zoom: 1
-  };
+	handleApiLoaded = (map, maps) => {
+		//alert('handleApiLoaded');
+		var legend = document.getElementById('legend');
+		map.controls[maps.ControlPosition.BOTTOM_CENTER].push(legend);
+		//drawMarkers(map, maps, ['No Reservations', 'The Layover', 'Parts Unknown']);
+		//
+		var marker = new maps.Marker({ position: {'lat': 27.84, 'lng': 11.11}, map });
+		this.setState({markers: [marker]});
+		//marker.setMap(map);
+		//marker.setMap(null);
+		//
+		let url = "https://raw.githubusercontent.com/alexanderjpowell/bourdain-destinations-react/master/src/all.json";
+		/*fetch(url).then(response => response.json()).then(json => {
+			var marker;
+			var infowindow = new maps.InfoWindow();
+			for (let i = 0; i < json.length; i++) {
+				//if (shows.includes(json[i].show)) {
+				let coords = json[i].coordinates.split(',');
+				let position = { lat: parseFloat(coords[0]), lng: parseFloat(coords[1]) };
+				marker = new maps.Marker({
+					position: position,
+					map,
+				});
+				maps.event.addListener(marker, 'click', (function(marker, i) {
+					return function() {
+						let showNameAndEpisode = json[i].show + ' Season ' + json[i].season + ', Episode ' + json[i].ep;
+						let title = json[i].title;
+						let state = (json[i].state === 'NA') ? '' : ', ' + json[i].state;
+						let location = json[i].city_or_area + state + ', ' + json[i].country;
+						let content = '<div><a href="' + json[i].source + '" target="_blank">' + showNameAndEpisode + ': ' + title + '</a></div>'
+								+ '<div>' + location + '</div>';
+						infowindow.setContent(content);
+						infowindow.open(map, marker);
+					}
+				})(marker, i));
+				//}
+			}
+		}).catch(error => {alert(error);});*/
+	};
 
-  handleNoReservationsClick() {
-	//alert('You clicked the Chip.');
-	drawMarkers()
-  };
+	componentDidMount() {
+	}
 
-  handleTheLayoverClick() {
+	static defaultProps = {
+		center: {
+			lat: 27.84,
+			lng: 11.11
+		},
+		zoom: 1
+	};
 
-  };
+	handleNoReservationsClick() {
+	};
 
-  handlePartsUnknownClick() {
+	handleTheLayoverClick = () => {
+		this.state.markers[0].setMap(null);
+	};
 
-  };
+	handlePartsUnknownClick() {
+	};
 
   render() {
     return (
-    <div>
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{key: MAPS_API_KEY.key }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}>
-        </GoogleMapReact>
-        <div id="legend">
-			<Grid container spacing={3}>
-				<Grid item xs={12}>
-					<Paper elevation={3} style={LegendStyle}>
-						{/*<Chip label="A Cook's Tour" variant="outlined" style={ChipStyle} onClick={this.handleClick}/>*/}
-						<Chip label="No Reservations" variant="outlined" style={ChipStyle} onClick={this.handleNoReservationsClick}/>
-						<Chip label="The Layover" variant="outlined" style={ChipStyle} onClick={this.handleTheLayoverClick}/>
-						<Chip label="Parts Unknown" variant="outlined" style={ChipStyle} onClick={this.handlePartsUnknownClick}/>
-					</Paper>
-				</Grid>
-			</Grid>
-        </div>
-      </div>
-    </div>
+		<div>
+			<div style={{ height: '100vh', width: '100%' }}>
+				<GoogleMapReact
+					bootstrapURLKeys={{key: MAPS_API_KEY.key }}
+					defaultCenter={this.props.center}
+					defaultZoom={this.props.zoom}
+					yesIWantToUseGoogleMapApiInternals
+					onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
+				>
+					
+					{/*{this.state.markers}*/}
+				</GoogleMapReact>
+				<div id="legend">
+					<Grid container spacing={3}>
+						<Grid item xs={12}>
+							<Paper elevation={3} style={LegendStyle}>
+								{/*<Chip label="A Cook's Tour" variant="outlined" style={ChipStyle} onClick={this.handleClick}/>*/}
+								<Chip label="No Reservations" variant="outlined" style={ChipStyle} onClick={this.handleNoReservationsClick}/>
+								<Chip label="The Layover" variant="outlined" style={ChipStyle} onClick={this.handleTheLayoverClick}/>
+								<Chip label="Parts Unknown" variant="outlined" style={ChipStyle} onClick={this.handlePartsUnknownClick}/>
+							</Paper>
+						</Grid>
+					</Grid>
+				</div>
+			</div>
+		</div>
     );
   }
 }
